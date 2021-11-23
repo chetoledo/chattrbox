75693ad734143125760a4f40a56a220b93deebd5
Juan Toledo <oneonet01@outlook.com>
2021-11-22 22:45:27 -0800

    Submitting directories for submission

---
+++ app/scripts/dist/main.js
@@ -0,0 +1,1 @@
+console.error("\n/home/senor-sol/Documents/Front_End/github_chattrbox/app/scripts/src/main.js:1\nimport ChatApp from './app/scripts/src/dom.js';\n^\nParseError: 'import' and 'export' may appear only with 'sourceType: module'");
---
+++ app/scripts/src/main.js
@@ -0,0 +1,2 @@
+import ChatApp from './app/scripts/src/dom.js';
+new ChatApp();
